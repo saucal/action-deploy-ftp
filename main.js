00c0eb546627011d14e2a1a5531299a75f308172
Matias Saggiorato <matiassaggiorato@gmail.com>
2022-11-18 20:13:10 -0300

    Final implementation of node action for ftp deployment

--- main.js
+++ main.js
@@ -2,11 +2,48 @@
 
 (async function() {
 
-	const exec = require('@actions/exec');
 	const core = require('@actions/core');
 	const path = require('path');
-	core.addPath( path.resolve( __dirname ) );
+	const ftpClient = require('ftp-deployment/client');
 
-	await exec.exec('main.sh', [ core.getInput('token', { required: true }) ]);
+	class cliFTPClient extends ftpClient {
+		constructor() {
+			super( {
+				type: core.getInput('env-type', { required: false }),
+				host: core.getInput('env-host', { required: true }),
+				port: core.getInput('env-port', { required: true }),
+				username: core.getInput('env-user', { required: true }),
+				password: core.getInput('env-pass', { required: true }),
+				remoteRoot: core.getInput('env-remote-root', { required: true }),
+				localRoot: core.getInput('env-local-root', { required: true }),
+				debug: core.isDebug() ? (msg) => {
+					if (msg.startsWith('CLIENT')) {
+						console.error(msg);
+					}
+				} : undefined,
+			} );
+		}
+	}
+
+
+	function readData() {
+		let input = core.getInput('manifest', { required: true });
+		if ( input.startsWith( "+" ) || input.startsWith( "-" ) ) {
+			return require('stream').Readable.from( [ input ] );
+		} else {
+			return fs.createReadStream( path.resolve( input ) )
+		}
+	}
+
+	const client = new cliFTPClient();
+
+	client
+		.maybeConnect()
+		.then( function() {
+			return client.process( readData() );
+		} )
+		.then( function() {
+			return client.end()
+		} )
 
 })()
