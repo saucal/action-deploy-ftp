e0dde0f51987457fdaaae365f1b8a0850fd6fd2d
Matias Saggiorato <matiassaggiorato@gmail.com>
2023-01-24 20:14:35 -0300

    More speed tests

--- run.sh
+++ run.sh
@@ -76,6 +76,9 @@ rclone mount "remote:${INPUT_ENV_REMOTE_ROOT}" "${GITHUB_WORKSPACE}/remote" --da
 
 echo "test" > "${GITHUB_WORKSPACE}/remote/file"
 
+dd if=/dev/zero of="${GITHUB_WORKSPACE}/remote/test-1M.bin" bs=1K count=1 oflag=dsync
+dd if=/dev/zero of="${GITHUB_WORKSPACE}/remote/test-1M.bin" bs=1K count=10 oflag=dsync
+dd if=/dev/zero of="${GITHUB_WORKSPACE}/remote/test-1M.bin" bs=1K count=100 oflag=dsync
 dd if=/dev/zero of="${GITHUB_WORKSPACE}/remote/test-1M.bin" bs=1M count=1 oflag=dsync
 dd if=/dev/zero of="${GITHUB_WORKSPACE}/remote/test-10M.bin" bs=1M count=10 oflag=dsync
 dd if=/dev/zero of="${GITHUB_WORKSPACE}/remote/test-100M.bin" bs=1M count=100 oflag=dsync
